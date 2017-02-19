8dace68646a73bd22bab51fbc97d3b2a7b944db7
naturomics <naturomics.liao@gmail.com>
2017-02-19 20:17:58 +0800

    visualize as gif

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main(_):
     #                 [dcgan.h4_w, dcgan.h4_b, None])
 
     # Below is codes for visualization
-    OPTION = 1
+    OPTION = 3
     visualize(sess, deepgan, FLAGS, OPTION)
 
 if __name__ == '__main__':
